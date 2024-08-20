8e39eac30a0a285319a1d16ca45369c10016a898
Keerthana D <144937763+KeerthanaD11@users.noreply.github.com>
2024-08-20 07:08:33 +0530

    Add files via upload

---
+++ primeNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class primeNumber{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		checkPrime(num);
+	}
+		private static void checkPrime(int num){
+			int count = 0;
+			if(num<=2){
+				System.out.println(num+ " is not a prime number");
+			}
+			else{
+				for(int i=2; i<=num; i++){
+					if(num%i == 0){
+						count++;
+					}
+				}
+				if(count>=2){
+					System.out.println(num+ " is not a prime number.");
+				}
+				else{
+					System.out.println(num+ " is a prime number.");
+
+				}
+			}
+		}
+		
+}
