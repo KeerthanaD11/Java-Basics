bdc429a0f07bf6a2bf1374ad4cc573e84cc1382c
Keerthana D <144937763+KeerthanaD11@users.noreply.github.com>
2024-09-09 22:40:26 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+/*public class Patterns1 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+public class Pattern1{
+    public static void main(String[] args) {
+        int col = 3;
+        int row = 4;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
